13e81ae48b9bfca9f33715ec08287020d3073f07
Golfantara <golfantara7@gmail.com>
2023-06-13 10:43:31 +0700

    added-produk-pages

---
+++ src/components/ProdukPages/FirstSection/index.jsx
@@ -0,0 +1,22 @@
+const FirstSection = () => {
+  return (
+    <div>
+      <div className="container mt-24 md:mt-36">
+        <span className="grid text-center text-merah mb-6 text-base font-semibold">
+          Produk Kami
+        </span>
+        <span className="grid text-center text-4xl font-semibold mb-4">
+          Super Herbs High Tech
+        </span>
+        <span className="grid text-center text-xl text-[#667085]">
+          Nikmati manfaat yang optimal dengan produk herbal kami yang aman
+        </span>
+        <span className="grid text-center text-xl text-[#667085]">
+          dan terpercaya. Pesan sekarang dan rasakan perbedaanya!
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default FirstSection;
---
+++ src/modules/ProdukPages/index.jsx
@@ -0,0 +1,12 @@
+import FirstSection from "@/components/ProdukPages/FirstSection";
+import SecondSection from "@/components/ProdukPages/SectondSection";
+const ModulesProduk = () => {
+  return (
+    <div>
+      <FirstSection />
+      <SecondSection />
+    </div>
+  );
+};
+
+export default ModulesProduk;
