cb221aa831316b3faa3dfe5580b215e709fcee9b
Golfantara <golfantara7@gmail.com>
2023-08-15 14:53:19 +0700

    improvement:animate-aos&&accordion-for-faq

--- src/components/FaqPages/layouts/index.jsx
+++ src/components/FaqPages/layouts/index.jsx
@@ -1,29 +1,54 @@
-const Card = ({ title, description }) => (
-  <div className="full-shadow rounded-lg grid lg:block m-2 p-2 lg:p-4">
-    <span className="grid text-xl text-hitam">{title}</span>
-    <span className="grid py-2 lg:py-4 text-[#667085] text-base">
-      {description}
-    </span>
-    <button className="flex justify-center items-center gap-2 bg-[#12B76A] px-3 py-2 rounded-lg">
-      <span>
-        <svg
-          width="30px"
-          height="30px"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-            stroke="#ffffff"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+import React, { useState } from "react";
+
+const Card = ({
+  title,
+  description,
+  detailedDescription,
+  isOpen,
+  onToggle,
+}) => {
+  const handleClose = () => {
+    if (isOpen) {
+      // Only toggle if it's currently open
+      onToggle();
+    }
+  };
+
+  return (
+    <div
+      className="full-shadow rounded-lg grid lg:block h-fit m-2 p-2 lg:p-4"
+      onMouseLeave={handleClose} // Added this line
+    >
+      <span className="grid text-xl text-hitam">{title}</span>
+      <span className="grid py-2 lg:py-4 text-[#667085] text-base">
+        {description}
       </span>
-      <span className="text-white">Lihat details</span>
-    </button>
-  </div>
-);
+      <button
+        className="flex justify-center items-center gap-2 bg-[#12B76A] px-3 py-2 rounded-lg"
+        onClick={onToggle}
+      >
+        <span>
+          <svg
+            width="30px"
+            height="30px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+              stroke="#ffffff"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </span>
+        <span className="text-white">Lihat details</span>
+      </button>
+      {isOpen && <p>{detailedDescription}</p>}
+    </div>
+  );
+};
+
 export default Card;
--- src/components/FaqPages/secondSection/index.jsx
+++ src/components/FaqPages/secondSection/index.jsx
@@ -1,16 +1,29 @@
 import Card from "../layouts";
 import cardData from "../cardData";
+import { useState } from "react";
 
 const secondSection = () => {
+  const [openCard, setOpenCard] = useState([]);
+  const toggleCard = (index) => {
+    if (openCard.includes(index)) {
+      setOpenCard((prev) => prev.filter((item) => item !== index));
+    } else {
+      setOpenCard((prev) => [...prev, index]);
+    }
+  };
+
   return (
     <div className="bg-[#F9FAFB]">
       <div className="lg:container px-3">
         <div className="grid grid-cols-2 gap-y-4 md:gap-y-6 lg:gap-y-8 lg:grid-cols-3 py-6 md:py-8 lg:py-12">
           {cardData.map((card, index) => (
             <Card
-              key={index}
+              key={`${card.id}-${openCard.includes(index)}`} // Corrected this line
               title={card.title}
               description={card.description}
+              detailedDescription={card.detailedDescription}
+              isOpen={openCard.includes(index)}
+              onToggle={() => toggleCard(index)}
             />
           ))}
         </div>
